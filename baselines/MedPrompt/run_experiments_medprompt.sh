ff69897b1abef22c04c1e732768c047538fa3e34
Daniel Shao <19307110036@fudan.edu.cn>
2025-02-15 21:19:13 +0000

    [dataset] reorganiaze folder structure

--- baselines/MedPrompt/run_experiments_medprompt.sh
+++ baselines/MedPrompt/run_experiments_medprompt.sh
@@ -10,16 +10,16 @@
 LOGS_DIR=logs
 DATA_DIR=../../data
 
-for model in gpt-35-turbo gpt-4 claude-3-5-haiku claude-3-5-sonnet o1-mini o3-mini gpt-4o-mini gpt-4o Qwen/QwQ-32B-Preview deepseek-ai/DeepSeek-R1 deepseek-ai/DeepSeek-V3 meta-llama/Llama-3.3-70B-Instruct-Turbo; do
+for model in gpt-4o-mini gpt-4o deepseek-ai/DeepSeek-V3; do
     {
         for dataset in medqa medmcqa pubmedqa medbullets mmlu-pro mmlu afrimedqa; do
-        mkdir -p $LOGS_DIR/$dataset 
-            for split in test; do
+            mkdir -p $LOGS_DIR/$dataset 
+            for split in test_hard; do
                 echo "Running $model on $dataset $split"
                 model_filename=$(echo $model | tr '/' '_')
                 log_file=$LOGS_DIR/$dataset/${model_filename}_${dataset}_${split}.log
                 error_file=$LOGS_DIR/$dataset/${model_filename}_${dataset}_${split}.err
-                python prob.py --dataset_name $dataset --dataset_dir $DATA_DIR/$dataset/ --split $split --model $model --output_files_folder ../../output/ --num_processes 4 > $log_file 2> $error_file
+                python medprompt.py --dataset_name $dataset --dataset_dir $DATA_DIR/$dataset/ --split $split --model $model --output_files_folder ../../output/ --num_processes 4 --num_rounds 3 --vote_count 3 > $log_file 2> $error_file
             done
         done
     } &
